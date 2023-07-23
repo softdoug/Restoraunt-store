23b6eeda8e3e457d1a3de903acae99d6cbac2350
Softdoug <lev.vasilenko@gmail.com>
2023-07-23 13:37:26 +0300

    dev search panel

--- script.js
+++ script.js
@@ -22,23 +22,27 @@ const filter = document.querySelector('#filter')
 const container = document.querySelector('.grid-container')
 
 filter.addEventListener('input', (event) => {
-    console.log('inp')
-    // let value = event.target.value.toLowerCase()
-    // const filterdRests = allRest.filter((rest) => {
-    //     return rest.restName.toLowerCase().includes(value)
-    // })
-    // render(filterdRests)
+    let value = event.target.value.toLowerCase()
+    const filtredRests = allRest.filter(rest => {
+        return  rest.restName.toLowerCase().includes(value)
+    })
+    console.log(filtredRests[0].restName);
+    render(filtredRests)
 })
 
 function render(rest) {
-    container.innerHTML = `
-    div class="store-card">
-            <img src="img/${rest.img}">
-            <h5 class="rest-name">${rest.restName}</h5>
-            <p class="kitchen">${restKitchen}</p>
-            <p>25-35 мин</p>
-        </div>
-    `
+    container.innerHTML = ''
+    for (key in rest) {
+        console.log(rest[key].restName);
+        container.innerHTML += `
+        div class="store-card">
+                <img src="#">
+                <h5 class="rest-name">${rest[key].restName}</h5>
+                <p class="kitchen">${rest[key].restKitchen}</p>
+                <p>25-35 мин</p>
+            </div>
+        `
+    }
 }
 
-console.log(container)
+// console.log(container)
